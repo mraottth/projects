646b2590d25cbb8695b89ab3c9af29d63276969c
Matt Roth <matthew.adam.roth@gmail.com>
2023-11-25 18:43:49 -0500

    first version of csv upload in js script

--- Goodreads Book Recommender/Web App/js/main.js
+++ Goodreads Book Recommender/Web App/js/main.js
@@ -1,1 +1,27 @@
-console.log("let's get started!")
+let jsonData; // Variable to store the parsed JSON data
+
+function handleFile() {
+    const fileInput = document.getElementById('csvFileInput');
+
+    const file = fileInput.files[0];
+
+    if (file) {
+        // Use PapaParse to parse the CSV file
+        Papa.parse(file, {
+            header: true,
+            dynamicTyping: true,
+            complete: function(results) {
+                // results.data contains the parsed CSV data as an array of objects
+                jsonData = results.data;
+
+                // Now you can use jsonData as a JavaScript variable
+                console.log('Parsed JSON data:', jsonData);
+            },
+            error: function(error) {
+                console.error('CSV parsing error:', error.message);
+            }
+        });
+    } else {
+        alert('Please select a CSV file.');
+    }
+}
