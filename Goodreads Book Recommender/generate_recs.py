e69f9f36f50f9b7add2e86df0a7f7f8b19593a42
Matt Roth <matthew.adam.roth@gmail.com>
2023-11-29 16:59:49 -0500

    changed filepaths in classes and generate_recs

--- Goodreads Book Recommender/generate_recs.py
+++ Goodreads Book Recommender/generate_recs.py
@@ -2,5 +2,5 @@ from recommender_classes import BookRecommender, BookLoader
 import pandas as pd 
 if __name__ == '__main__':
     wd = os.getcwd()
-    df = pd.read_csv(wd + "/data/goodreads_library_export.csv")
+    df = pd.read_csv(wd + "/Web App/data/goodreads_library_export.csv")
     recs = BookRecommender(df)
