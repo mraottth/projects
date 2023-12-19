d1a3466d79c330f95572cb88077cdfbbe1bfc567
Matt Roth <matthew.adam.roth@gmail.com>
2023-12-19 11:08:27 -0500

    removed generate_recs file

--- Goodreads Book Recommender/generate_recs.py
+++
@@ -1,6 +0,0 @@
-from recommender_classes import BookRecommender, BookLoader
-import pandas as pd 
-if __name__ == '__main__':
-    wd = os.getcwd()
-    df = pd.read_csv(wd + "/Web App/data/goodreads_library_export.csv")
-    recs = BookRecommender(df)
